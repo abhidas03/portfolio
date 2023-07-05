e6e6542abf1006f3920d291f61d19b3d1dae0d63
Abhi <abhijit.das3002@gmail.com>
2023-07-05 12:28:42 -0400

    so far

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className='About'>
+
+        <div className='Header'>
+          <p>Hey! Welcome to my Portfolio!</p>
+        </div>
+
+        <div className='aboutText'>
+          <p>My name is Abhi, and I’m a Software Engineer. I’m a sophomore at Swarthmore College, currently doing machine learning research full-time over the Summer! </p>
+        </div>
+
+      </div>
+
+      <div className='showcase'>
+        <div className="projects">
+          <div className="projects1"></div>
+          <div className="projects2"></div>
+          <div className="projects3"></div>
+        </div>
+
+      </div>
+
     </div>
+    
   );
 }
 
